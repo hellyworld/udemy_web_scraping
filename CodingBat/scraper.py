c747adaec7d7dd7bba67c4e1076a6622f3060fdb
hellyworld <adrian.helerea@gmail.com>
2019-03-13 09:12:00 +0200

    CodingBat scraper

--- CodingBat/scraper.py
+++ CodingBat/scraper.py
@@ -1,25 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
-
+# from fake_useragent import UserAgent
+#
+# user_agent = UserAgent
+# headers = {'user-agent': user_agent.chrome}
 target_link = 'https://codingbat.com'
 
 
 def soup_maker(link):
-    return BeautifulSoup(requests.get(link).content, 'html.parser')
+    # return BeautifulSoup(requests.get(link).content, 'html.parser')
+    return BeautifulSoup(requests.get(link).content, 'lxml')
 
 
-target_soup = soup_maker(target_link)
-categories_links = [target_link + div.a['href'] for div in target_soup.find_all('div', class_='summ')]
+first_soup = soup_maker(target_link)
+categories_links = [target_link + div.a['href'] for div in first_soup.find_all('div', class_='summ')]
 
-print(categories_links)
-print(soup_maker('https://codingbat.com/java/Warmup-1').find_all('indent'))
-problems_links = []
 for category_link in categories_links:
     target_soup = soup_maker(category_link)
-    target = target_soup.find('div', class_='indent')
-    for td in target.find_all('td'):
-        print(td)
-    # problems_links.append([target_link + table.tbody.tr.td.a['href'] for table in target_soup.find_all('table')])
-    # print(problems_links)
+    div = target_soup.find('div', class_='indent')
+    problems_links = [target_link + td.a['href'] for td in div.table.find_all('td')]
 
-# print(problems_links)
+    for problem_link in problems_links:
+        final_soup = soup_maker(problem_link)
+        question = final_soup.find('div', class_='indent')
+        print(question.table.div.string)
+
+        break
+
+    break
+
